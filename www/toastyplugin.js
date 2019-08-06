ea231c2ba882ba28e396ad5776c9dc274f613e65
Jhensson Frank Ayma Flores <jhensson.ayma@gmail.com>
2019-08-06 13:50:48 -0500

    as

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -3,11 +3,11 @@ function ToastyPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-ToastyPlugin.prototype.show3 = function(message, duration, successCallback, errorCallback) {
+ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
   options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show2', [options]);
+  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
 }
 
 // Installation constructor that binds ToastyPlugin to window
