dab31530dc54479e6e7af22316f5c594c530267c
Jhensson Frank Ayma Flores <jhensson.ayma@gmail.com>
2019-08-06 13:18:02 -0500

    Commited3

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -3,7 +3,7 @@ function ToastyPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+ToastyPlugin.prototype.show3 = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
   options.duration = duration;
